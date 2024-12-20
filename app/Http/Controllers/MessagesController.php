d3105517e8060f5ced01f4f27fe4cc7e0ddc1ea5
hunterbrightdesign <70662742+hunterbrightdesign@users.noreply.github.com>
2024-12-20 04:14:03 +0100

    correct user

--- app/Http/Controllers/MessagesController.php
+++ app/Http/Controllers/MessagesController.php
@@ -614,8 +614,10 @@ class MessagesController extends Controller
         $newdateone = strtotime(Date('Y-m-d H:i:s'));
         $hello = abs( $newdateone - $newdate)/120;
         $tag = Liste_tag::where('id_user', Auth::user()->id)->first();
-        $settmessage = 0;
-        if($tag->tmessage){
+        $settmessage = null;
+        if(empty($tag->tmessage)){
+            $settmessage = 0;
+        }else{
             $settmessage = $tag->tmessage;
         }
         $tmessage = $tag->tmessage +1;
